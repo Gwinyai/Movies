17eb5d45eb5671f73cd94944f524304e6ae29939
Gwinyai <gnyatsoka@gmail.com>
2022-04-11 13:36:03 +0200

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Gwinyai Nyatsoka on 7/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
