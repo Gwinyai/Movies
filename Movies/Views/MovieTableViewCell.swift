dac539f1fd3e8994318b395243cd6f435d7af209
Gwinyai <gnyatsoka@gmail.com>
2022-04-11 15:02:24 +0200

    Added tableview datasource and delegate

---
+++ Movies/Views/MovieTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieTableViewCell.swift
+//  Movies
+//
+//  Created by Gwinyai Nyatsoka on 11/4/2022.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieDescriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
